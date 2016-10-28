9abcf1b255b6ca5ece0e5410b87ec9bd2dcc4e1f
Jakub Staroń <staron.jk@gmail.com>
2016-10-28 21:49:40 +0200

    Adds tie(nullptr) to increase io performance.

--- main.cc
+++ main.cc
@@ -29,6 +29,8 @@ private:
 
 int main(int argc, const char* argv[]) {
   std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
   logger.set_log_level(logging::log_level::debug);
   logger.add_stream(&std::cerr, logging::log_level::debug);
   Application application;
