820f95775693d59587864cc372402175e12cfe32
Jakub Staroń <staron.jk@gmail.com>
2016-10-31 13:31:18 +0100

    Adds MinimalStringRotation algorithm.

---
+++ tests/minimal_string_rotation_test.cc
@@ -0,0 +1,57 @@
+// Jakub Staroń, 2016
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+#include "io.h"
+#include "text_algorithms/minimal_string_rotation.h"
+
+using namespace lib;
+
+BOOST_AUTO_TEST_SUITE(minimal_string_rotation_test)
+
+BOOST_AUTO_TEST_CASE(minimal_string_rotation_test) {
+  {
+    std::string text = "";
+    std::string expected = "";
+    BOOST_CHECK_EQUAL(text::MinimalStringRotation(text), expected);
+  }
+
+  {
+    std::string text = "a";
+    std::string expected = "a";
+    BOOST_CHECK_EQUAL(text::MinimalStringRotation(text), expected);
+  }
+
+  {
+    std::string text = "ba";
+    std::string expected = "ab";
+    BOOST_CHECK_EQUAL(text::MinimalStringRotation(text), expected);
+  }
+
+  {
+    std::string text = "cba";
+    std::string expected = "acb";
+    BOOST_CHECK_EQUAL(text::MinimalStringRotation(text), expected);
+  }
+
+  {
+    std::string text = "aabaaabaabaca";
+    std::string expected = "aaabaaabaabac";
+    BOOST_CHECK_EQUAL(text::MinimalStringRotation(text), expected);
+  }
+
+  {
+    std::string text = "baaabaabacaaa";
+    std::string expected = "aaabaaabaabac";
+    BOOST_CHECK_EQUAL(text::MinimalStringRotation(text), expected);
+  }
+
+  {
+    std::string text = "algorithm";
+    std::string expected = "algorithm";
+    BOOST_CHECK_EQUAL(text::MinimalStringRotation(text), expected);
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ text_algorithms/minimal_string_rotation.h
@@ -0,0 +1,82 @@
+#pragma once
+// Jakub Staroń, 2016
+
+#include "headers.h"
+
+namespace lib {
+namespace text {
+
+/**
+ * Computes lexicographically minimal sequence rotation.
+ *
+ * Returns iterator to position where rotation begins.
+ * Iterator must be random-access iterator.
+ * type of *Iterator must be comparable.
+ *
+ * Example:
+ * <pre>
+ * std::string s = "cba";
+ * MinimalStringRotation(s.begin(), s.end()); // returns iterator pointing to 'a'
+ * </pre>
+ */
+template <typename Iterator>
+Iterator MinimalStringRotation(Iterator begin, Iterator end) {
+  static_assert(
+      std::is_same<
+          typename std::iterator_traits<Iterator>::iterator_category,
+          std::random_access_iterator_tag>::value,
+      "Iterator must be random access iterator!"
+  );
+
+  using size_type = typename std::iterator_traits<Iterator>::difference_type;
+  const size_type length = std::distance(begin, end);
+
+  size_type i = 0;
+  size_type j = 1;
+
+  size_type k = 1;
+  size_type p = 1;
+
+  while (j + k <= (2 * length)) {
+    const auto& a = begin[(i + k - 1) % length];
+    const auto& b = begin[(j + k - 1) % length];
+    if (a > b) {
+      i = j++;
+      k = p = 1;
+    }
+    else if (a < b) {
+      j += k;
+      k = 1;
+      p = j - i;
+    }
+    else if (a == b && k != p) {
+      k++;
+    }
+    else {
+      j += p;
+      k = 1;
+    }
+  }
+  return begin + i;
+}
+
+/**
+ * Computes lexicographically minimal string rotation.
+ *
+ * Example:
+ * <pre>
+ * std::string s = "cba";
+ * MinimalStringRotation(s); // returns "acb"
+ * </pre>
+ */
+std::string MinimalStringRotation(const std::string& text) {
+  std::string result;
+  result.reserve(text.size());
+  auto it = MinimalStringRotation(text.begin(), text.end());
+  result.append(it, text.end());
+  result.append(text.begin(), it);
+  return result;
+}
+
+} // namespace text
+} // namespace lib
